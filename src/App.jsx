8599bda8faa23ecf985cdc82f1b9beefc7fc4cb9
Tariqsh123 <innoxent.tariq2016@gmail.com>
2026-04-23 17:24:06 +0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import ModulePage from "./pages/ModulePage";
 import './App.css'
 
 export default function App() {
   return (
     <>
       <Navbar />
-      
+      <Home/>
+
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/module/:slug" element={<ModulePage />} />
+        <Route path="/" element={<Home/>} />
         <Route path="/nurse-training" element={<div>Nurse Training</div>} />
         <Route path="/solutions" element={<div>Solutions</div>} />
         <Route path="/blog" element={<div>Blog</div>} />
